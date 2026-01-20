2ca3043fd5fa9c35dac90e3437037570c757bc3b
tokytoky <toky.sigogneau@fms-ea.com>
2026-01-20 11:40:03 +0100

    updated class chargerCard() to take multiple categories at once

--- script.js
+++ script.js
@@ -1,26 +1,39 @@
-async function chargerCards(choice) {
+
+//This function serve to fetch the date from the JSON file 
+//It takes a table of "categories" as parameters and will display items in each categories
+async function chargerCards(categories) {
   const response = await fetch('bonboncandy.json');
   const data = await response.json();
 
   const container = document.getElementById('produit');
+  container.innerHTML = ""; // vider avant affichage
 
-  data.bonbons.forEach(choice => {
-    const card = document.createElement('div');
-    card.classList.add('card');
+  categories.forEach(categorie => {
+    if (!data[categorie]) return; //if the result is empty, it will return nothing and stop the function
 
-    card.innerHTML = `
-      <img src="img/${choice.img}" alt="${choice.name}">
-      <h4>${choice.name}</h4>
-      <h4>${'⭐'.repeat(choice.score)}</h4>
-      <div class="prix">${choice.price} €</div>
-      <button>Ajouter au panier</button>
-    `;
+    data[categorie].forEach(choice => {
+      const card = document.createElement('div');
+      card.classList.add('card');
 
-    container.appendChild(card);
+      card.innerHTML = `
+        <img src="img/${choice.img}" alt="${choice.name}">
+        <h4>${choice.name}</h4>
+        <h4>${'⭐'.repeat(choice.score)}</h4>
+        <div class="prix">${choice.price} €</div>
+        <button>Ajouter au panier</button>
+      `;
+
+      container.appendChild(card);
+    });
   });
 }
 
-chargerCards("bonbon");
+//chargerCards(["bonbons", "boissons"]);
+
+const selections = ["bonbons", "boissons"];
+chargerCards(selections);
 
 
-// ,rkky
+
+
+
