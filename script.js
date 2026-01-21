a52cf06fa95edd2b16b7b95d610b3e631b59c93e
tokytoky <toky.sigogneau@fms-ea.com>
2026-01-21 13:58:09 +0100

    added functions to manipulate the cart, Need to fix chargerCartCard

--- script.js
+++ script.js
@@ -23,18 +23,91 @@ async function chargerCards(categories) {
         <div class="prix">${choice.price} €</div>
         </div>
 
-        <button>Ajouter au panier</button>
+        <button onclick="addToCart('${choice.name}')">Ajouter au panier</button>
       `;
+      
 
       container.appendChild(card);
     });
   });
 }
 
+
+//This function is used to display the element inside the user_cart[] variable 
+
+async function chargerCartCards() {
+  const response = await fetch('bonboncandy.json');
+  const data = await response.json();
+
+  const container = document.getElementById('cart_content');
+  container.innerHTML = ""; // vider avant affichage
+
+    if (!user_cart || user_cart.length === 0) return;
+
+    user_cart.forEach(name => {
+      const choice = data.find(item => item.name === name);
+      if (!choice) return;
+
+      const cart_card = document.createElement('div');
+      cart_card.classList.add('cart_card');
+
+      cart_card.innerHTML = `
+        <div class="cart_card_img">
+          <img src="img/${choice.img}" height="100px" alt="">
+        </div>
+
+        <div class="cart_card_2nd_part">
+          <a href="link-to-article">${choice.name}</a>
+          <div class="add_more_product">
+            <button>-</button>
+            <p>1</p>
+            <button>+</button>
+          </div>
+        </div>
+
+        <div class="cart_card_3rd_part">
+          <button>
+            <img src="img/poubelle.png" width="10px" alt="">
+          </button>
+          <h4 class="cart_product_price">${choice.price} €</h4>
+        </div>
+      `;
+
+      container.appendChild(cart_card);
+    });
+  
+}
+
+
+
+
 //chargerCards(["bonbons", "boissons"]);
 
 const selections = ["bonbons", "boissons"];
 chargerCards(selections);
+
+const user_cart = ["sucette rouge"]
+chargerCartCards()
+
+//Add an item to cart
+function addToCart(product){ 
+  user_cart.push(product)
+}
+
+//remove specific item from the cart
+function removeFromCart(product){
+
+}
+
+
+//Empty the cart
+function emptyCart(){
+  user_cart = []
+}
+
+
+
+
 
 
 
